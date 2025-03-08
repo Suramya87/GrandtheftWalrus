7aecc4c4f080d078e1045612a90e3948a6a459c4
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-03-07 21:02:30 -0800

    cops death and friction update

--- src/Play.js
+++ src/Play.js
@@ -7,84 +7,62 @@ class Play extends Phaser.Scene {
         this.SPEED_MULTIPLIER = 1;
         this.PLAYER_VELOCITY = 350;
         this.followerSpeed = 100 / this.SPEED_MULTIPLIER;
-        this.CHASE_VELOCITY = 500 / this.SPEED_MULTIPLIER;
+        this.CHASE_VELOCITY = 200 / this.SPEED_MULTIPLIER;
         this.player_isTouching = false;
         this.player_isTurning = false;
         this.ROTATION_SPEED = 2;
         this.LANES = false;
-        this.laneHeight = 960;
-        this.laneWidth = 8;
         this.timeSurvived = 0;
         this.highScore = localStorage.getItem('highScore') || 0;
         this.isGameOver = false;
-            
+
+        this.activeCops = []; // Store cops in the same scene
     }
 
     preload() {
-        this.load.spritesheet('character', './assets/testcar2.png', {
-            frameWidth: 128
-        });
-        this.load.image('test','./assets/kidnap.png')
-        this.load.tilemapTiledJSON('testJSON','./assets/temp_test.json');
-
+        this.load.spritesheet('character', './assets/testcar2.png', { frameWidth: 128 });
+        this.load.image('test', './assets/kidnap.png');
+        this.load.tilemapTiledJSON('testJSON', './assets/temp_test.json');
+        this.load.spritesheet('COPS', './assets/POLICE2.png', { frameWidth: 100 });
     }
 
     create() {
-        const map = this.add.tilemap('testJSON')
-        const tileset =  map.addTilesetImage('temp_test','test')
+        const map = this.add.tilemap('testJSON');
+        const tileset = map.addTilesetImage('temp_test', 'test');
+        const bgLayer = map.createLayer('BG',tileset,0,0)
+        this.footpathLayer = map.createLayer('Footpath', tileset, 0, 0);
+        this.KILLLayer = map.createLayer('KILL', tileset, 0, 0);
+
+        this.footpathLayer.setCollisionByProperty({ collides: true });
+        this.KILLLayer.setCollisionByProperty({ collides: true });
+
+    
+        this.enemySpawns = map.getObjectLayer('COPS').objects;
+
+      
+        const Walrus_spawn = map.findObject('Spawn', (obj) => obj.name === 'Walrus spawn');
+        this.player = this.physics.add.sprite(Walrus_spawn.x, Walrus_spawn.y, 'character', 1).setScale(0.5);
+        this.player.body.setCollideWorldBounds(true);
+        this.player.setSize(56, 64);
+        this.player.body.setBounce(2);
+        this.isCooldown = false;
+        this.cooldownTime = 2000;
+
+    
+        this.cameras.main.startFollow(this.player, false, 0.5, 0.5);
+        this.cameras.main.setZoom(3);
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
         
-        const bgLayer = map.createLayer('Tile Layer 1',tileset,0,0)
-        const FootpathLayer = map.createLayer('Footpath',tileset,0,0)
-        
-        FootpathLayer.setCollisionByProperty({collides: true})        
-        const Walrus_spawn = map.findObject('Spawn', (obj) => obj.name === 'Walrus spawn')
-        
+        this.physics.add.collider(this.player, this.footpathLayer);
 
-
-        const PLAYER = () => {
-            this.player = this.physics.add.sprite(Walrus_spawn.x, Walrus_spawn.x, 'character', 1).setScale(0.5);
-            this.player.body.setCollideWorldBounds(true);
-            this.player.setSize(56, 64);
-            this.player.body.setBounce(2)
-            this.isCooldown = false;
-            this.cooldownTime = 2000;
-            this.player_isTouching = false;
-
-        };
-        
-        PLAYER();
-        this.cameras.main.setBounds(0,0,map.widthInPixels,map.heightInPixels)
-        this.cameras.main.startFollow(this.player,true,0.25,0.25)
-
-        this.physics.world.setBounds(0,0,map.widthInPixels,map.heightInPixels)
-
-        this.physics.add.collider(this.player, FootpathLayer)
-
-
-        this.timeEvent = this.time.addEvent({
-            delay: 1000, 
-            callback: () => {
-                this.timeSurvived++;
-                this.timeText.setText(`Time: ${this.timeSurvived}s`);
-            },
-            loop: true
+      
+        this.input.keyboard.on('keydown-SPACE', () => {
+            this.spawnCop();
         });
 
-
-        this.timeText = this.add.text(20, 20, `Time: ${this.timeSurvived}s`, {
-            fontSize: '32px',
-            fill: '#ffffff',
-            fontFamily: 'Arial',
-            fontWeight: 'bold'
-        }).setScale(0.33);
-
-
-        this.highScoreText = this.add.text(20, 60, `High Score: ${this.highScore}s`, {
-            fontSize: '32px',
-            fill: '#ffffff',
-            fontFamily: 'Arial'
-        }).setScale(0.33);
-
+ 
+        this.cursors = this.input.keyboard.createCursorKeys();
 
         this.anims.create({
             key: 'normal',
@@ -125,84 +103,133 @@ class Play extends Phaser.Scene {
                 end: 3
             })
         });
+        // Create cop animations
+        this.anims.create({
+            key: 'chillin',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('COPS', { start: 0, end: 0 })
+        });
 
-        cursors = this.input.keyboard.createCursorKeys();
+        this.anims.create({
+            key: 'not-chillin',
+            frameRate: 15,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('COPS', { start: 1, end: 4 })
+        });
+
+        this.anims.create({
+            key: 'imma-head-out',
+            frameRate: 0,
+            repeat: -1,
+            frames: this.anims.generateFrameNumbers('COPS', { start: 5, end: 5 })
+        });
     }
-    gameOver() {
-        if (!this.isGameOver) {
-            this.isGameOver = true;  
+
+    spawnCop() {
+        if (this.enemySpawns.length === 0) {
+            console.warn("No police");
+            return;
+        }
     
-            
-            if (this.timeSurvived > this.highScore) {
-                this.highScore = this.timeSurvived;
-                localStorage.setItem('highScore', this.highScore);
+        const spawnPoint = Phaser.Utils.Array.GetRandom(this.enemySpawns);
+        const spawnX = spawnPoint.x + Phaser.Math.Between(-10, 10);
+        const spawnY = spawnPoint.y + Phaser.Math.Between(-10, 10);
+    
+        console.log(`WOOPWOOP: (${spawnX}, ${spawnY})`);
+    
+        const cop = this.physics.add.sprite(spawnX, spawnY, 'COPS', 0).setScale(0.5).setDepth(10);
+        cop.setSize(56, 64);
+        cop.setAngle(Phaser.Math.Between(0, 360));
+        cop.setCollideWorldBounds(true);
+        cop.body.setDrag(200);
+        cop.body.setFriction(0.1);
+    
+        this.physics.add.collider(cop, this.footpathLayer, () => {
+            console.log("BOOP");
+        });
+        
+        // Add collision between cop and KILL layer
+        this.physics.add.collider(cop, this.KILLLayer, () => {
+            this.destroyCop(cop);
+        });
+    
+        this.physics.add.collider(cop, this.player, () => {
+            console.log("Get rekt");
+            if (this.LANES) {
+                this.gameOver();
             }
+        });
     
-            
-            this.scene.start('gameOver', { 
-                timeSurvived: this.timeSurvived, 
-                highScore: this.highScore 
-            });
+        this.activeCops.forEach(otherCop => {
+            this.physics.add.collider(cop, otherCop);
+        });
+    
+        this.activeCops.push(cop);
+    }
+
+    destroyCop(cop) {
+        if (cop && cop.active) {
+            const index = this.activeCops.indexOf(cop);
+            if (index > -1) {
+                this.activeCops.splice(index, 1); // Remove from array
+            }
+            cop.destroy(); // Destroy sprite
+            console.log("Cop eliminated!");
         }
     }
     
+    
+
     update() {
-
+        //player
         if (!this.player.destroyed) {
-            if (!this.physics.world.overlap(this.player, this.lanes)) {
-                this.player_isTouching = false;
-            }
-
-            
             let playerVelocity = new Phaser.Math.Vector2(0, 0);
 
-            if (cursors.up.isDown) {
-                this.SPEED_MULTIPLIER = 2
-               
+            if (this.cursors.up.isDown) {
+                this.SPEED_MULTIPLIER = 2;
                 playerVelocity.y = -Math.cos(this.player.rotation) * this.PLAYER_VELOCITY;
                 playerVelocity.x = Math.sin(this.player.rotation) * this.PLAYER_VELOCITY;
                 this.player.play('speed');
-            } else if (cursors.down.isDown) {
-                this.SPEED_MULTIPLIER = 0.5
-                
+
+            } else if (this.cursors.down.isDown) {
+                this.SPEED_MULTIPLIER = 0.5;
                 playerVelocity.y = Math.cos(this.player.rotation) * (this.PLAYER_VELOCITY * 0.5);
                 playerVelocity.x = -Math.sin(this.player.rotation) * (this.PLAYER_VELOCITY * 0.5);
                 this.player.play('speed');
             }
 
-            if (cursors.left.isDown) {
-
-                this.player.angle -= this.ROTATION_SPEED; 
+            if (this.cursors.left.isDown) {
+                this.player.angle -= this.ROTATION_SPEED;
                 this.player.play('idle-left');
-                this.player_isTurning = true;
-            } else if (cursors.right.isDown) {
-               
-                this.player.angle += this.ROTATION_SPEED; 
+            } else if (this.cursors.right.isDown) {
+                this.player.angle += this.ROTATION_SPEED;
                 this.player.play('idle-right');
-                this.player_isTurning = true;
             }
 
-            if (!cursors.right.isDown && !cursors.left.isDown) {
+            if (!this.cursors.left.isDown && !this.cursors.right.isDown && !this.cursors.up.isDown && !this.cursors.down.isDown) {
                 this.player_isTurning = false;
-                
-            }
-
-            if (!cursors.up.isDown && !cursors.down.isDown && !cursors.right.isDown && !cursors.left.isDown) {
-                this.SPEED_MULTIPLIER = 1
                 this.player.play('normal');
             }
 
-            if (this.player_isTouching && !this.player_isTurning) {
-                this.LANES = true;
-            }
 
             this.player.setVelocity(playerVelocity.x, playerVelocity.y);
-            
-        // if (this.player.y > height + 100) {
-            // this.gameOver();
-        //   }
-    
         }
 
+
+        for (let i = this.activeCops.length - 1; i >= 0; i--) {
+            const cop = this.activeCops[i];
+
+            if (!this.player) continue;
+
+            let targetAngle = Phaser.Math.Angle.Between(cop.x, cop.y, this.player.x, this.player.y);
+            //dumbass cops at the moment but they will be breaking ankles at 5 stars
+            cop.rotation = Phaser.Math.Angle.RotateTo(cop.rotation, targetAngle, 0.013);
+
+            const speed = this.CHASE_VELOCITY;
+            cop.setVelocity(Math.cos(cop.rotation) * speed, Math.sin(cop.rotation) * speed);
+
+            cop.play('not-chillin');
+        }
     }
 }
